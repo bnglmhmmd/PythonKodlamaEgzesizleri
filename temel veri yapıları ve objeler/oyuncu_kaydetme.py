4f9ab2c49426385618cb73e135554d700ee19859
bnglmhmmd <100075711+bnglmhmmd@users.noreply.github.com>
2022-02-24 10:45:01 +0300

    Temel Veri Yapıları
    Example with python

---
+++ temel veri yapıları ve objeler/oyuncu_kaydetme.py
@@ -0,0 +1,14 @@
+print("Oyuncu Kaydetme Programı")
+
+ad = input("Oyuncunun adı:")
+soyad = input("Oyuncunun soyadı:")
+takım = input("oyuncunun takımı:")
+
+bilgiler=[ad,soyad,takım]
+
+print("Bilgiler Kaydediliyor..")
+
+print("Oyuncunun Adı:{}\n Oyuncunun Soyadı:{}\n Oyuncunun Takımı:{}\n".format(bilgiler[0],bilgiler[1],bilgiler[2] ) )
+
+print("Bilgiler Kaydedildi.")
+
